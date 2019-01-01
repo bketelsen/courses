3d101506aa90a9477113761f4c8738ece5122939
Brian Ketelsen <mail@bjk.fyi>
2019-01-01 10:53:49 -0500

    courses start

---
+++ gatsby-node.js
@@ -0,0 +1,53 @@
+const path = require("path");
+
+exports.createPages = async ({  actions, graphql }) => {
+  const { createPage } = actions;
+
+  const lessonTemplate = path.resolve(`src/templates/lessonTemplate.js`);
+
+  return graphql(`
+    {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___order] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            excerpt(pruneLength: 250)
+            html
+            id
+            frontmatter {
+              order
+              path
+              title
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      return Promise.reject(result.errors);
+    }
+
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.frontmatter.path,
+        component: lessonTemplate
+      });
+    });
+  });
+};
+
+exports.onCreatePage = async ({ page, actions }) => {
+  const { createPage } = actions
+
+  // page.matchPath is a special key that's used for matching pages
+  // only on the client.
+  if (page.path.match(/^\/app/)) {
+    page.matchPath = `/app/*`
+
+    // Update the page.
+    createPage(page)
+  }
+};
